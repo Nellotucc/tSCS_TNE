344357413352e6f5f8b09e03a98a5906e088df11
riccardoprog <92370991+riccardoprog@users.noreply.github.com>
2024-04-02 11:28:23 +0200

    reordered

--- MATLAB/DATA_ANALYSIS/data_analysis.m
+++ MATLAB/DATA_ANALYSIS/data_analysis.m
@@ -4,9 +4,14 @@ directories = {
     '/Users/riccardocarpineto/Documents/TNE_MA2/tSCS_TNE/MATLAB/DATA/Kelly/small_electrode/yellow3/',
     '/Users/riccardocarpineto/Documents/TNE_MA2/tSCS_TNE/MATLAB/DATA/Kelly/small_electrode/black/'
 };
-current_i = 20;
-current_f = 65;
+directories = {
+    '/Users/riccardocarpineto/Documents/TNE_MA2/tSCS_TNE/MATLAB/DATA/20march/yellow2/', 
+    '/Users/riccardocarpineto/Documents/TNE_MA2/tSCS_TNE/MATLAB/DATA/20march/black2/'
+};
+current_i = 25;
+current_f = 50;
 num_repetitions = 2;
 bool_plot = true;
+interpulse_duration = 100;
 
-x = best_electrode_finder(directories,current_i,current_f,50,num_repetitions,'conventional',bool_plot);
+x = best_electrode_finder(directories,current_i,current_f,interpulse_duration,num_repetitions,'conventional',bool_plot);
